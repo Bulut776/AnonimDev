1fae7fc74965f36939855885269d22b6bfe83aa7
Bulut776 <156075546+Bulut776@users.noreply.github.com>
2024-08-20 09:36:31 +0300

    index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,20 +10,20 @@ export default function Index() {
       <img width="500" src="/img/anonim.png" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          MusicMaker
+          Anonim-Dev
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
        Anonim-Dev Tarafından Tasarlanan bir site
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-            <Link href={"https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=applications.commands%20bot"}>
+            <Link href={"https://discord.gg/heraroleplay"}>
               <a
                 className={
                   "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
                 }
               >
-          Invite MusicMaker
+          Discord Server
               </a>
             </Link>
   <div className="py-10"></div>
@@ -48,7 +48,7 @@ Vote MusicMaker</a>
             <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"MusicMaker" }} />
             <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
             <a href="https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=bot%20applications.commands" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
-              <i className="fab fa-discord mr-2" />Invite MusicMaker
+              <i className="fab fa-discord mr-2" />Discord Server
             </a>
           </div>
 
